4197e27697beecfec79d1bfbe8a3824a17a279b9
nkriger <116672984+nkriger@users.noreply.github.com>
2023-04-20 16:04:19 -0700

    Basic controller

--- Final Project/Final Project/Assets/Scripts/PlayerController.cs
+++ Final Project/Final Project/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,29 @@ public class PlayerController : MonoBehaviour
 {
     //Create a working input system for camera and character movement
 
+    public float speed;
+    private PlayerInput playerInput;
+    private Rigidbody rigidBody;
 
 
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        playerInput = new PlayerInput();
+        playerInput.Enable();
+    }
+    private void Update()
+    {
+        movement();
+    }
 
     void movement()
+    {
+        Vector3 moveVec = playerInput.Movement.WASD.ReadValue<Vector3>();
+        transform.Translate(moveVec * speed * Time.deltaTime);
+    }
+
+    void mouseCheck()
     {
 
     }
